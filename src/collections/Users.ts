ec46409c0322e99b78018b1632b85ddac3ecf1d2
Nils Weber <me@vimkat.dev>
2023-02-24 22:46:55 +0100

    feat(users): Allow empty telephone number

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -127,10 +127,12 @@ const Users: CollectionConfig = {
 							name: "telephone",
 							label: "Telefon",
 							type: "text",
-							validate: (val) =>
-								/\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/.test(
-									val
-								) || "Telefonnummer ist falsch formatiert",
+							validate: (val, args) =>
+								val !== ""
+									? /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/.test(
+											val
+									  ) || "Telefonnummer ist falsch formatiert"
+									: text(val, args),
 						},
 					],
 				},
